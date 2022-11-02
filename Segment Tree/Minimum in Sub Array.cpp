ee1bf9adc4b283b96f5d87bec1ca7762b85746f2
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-11-02 10:03:31 +0530

    Update and rename Minimum in Sub Array to Minimum in Sub Array.cpp

--- Segment Tree/Minimum in Sub Array.cpp
+++ Segment Tree/Minimum in Sub Array.cpp
@@ -1,37 +1,37 @@
-Minimum In SubArray
+// Minimum In SubArray
 
-You are given a sequence A[1], A[2], ..., A[N] of N numbers ( 0 ≤ A[i] ≤ 10^8 , 2 ≤ N ≤ 10^5 ). 
-There are Q queries, and each query is defined in either of the following two ways:
-Query on range:
-You are given two numbers i and j; the answer to each query is the minimum number between the range (i, j) (including both i and j). 
-Note that in this query, i and j lies in the range: [1, N].
-Update query:
-You are given two numbers i and B; update A[i] to B. Note that in this query, i lies in the range: [1, N].
-Input Format:
-The first line of input contains two integers: N and Q, representing the length of the sequence and the number of queries. 
-The second line of input contains N space-separated integers, A[i].  
-Next, Q lines contain the queries. Each line contains one character, followed by two space-separated integers. 
-For the query on range, the character will be q and for the update query, the character will be u. 
+// You are given a sequence A[1], A[2], ..., A[N] of N numbers ( 0 ≤ A[i] ≤ 10^8 , 2 ≤ N ≤ 10^5 ). 
+// There are Q queries, and each query is defined in either of the following two ways:
+// Query on range:
+// You are given two numbers i and j; the answer to each query is the minimum number between the range (i, j) (including both i and j). 
+// Note that in this query, i and j lies in the range: [1, N].
+// Update query:
+// You are given two numbers i and B; update A[i] to B. Note that in this query, i lies in the range: [1, N].
+// Input Format:
+// The first line of input contains two integers: N and Q, representing the length of the sequence and the number of queries. 
+// The second line of input contains N space-separated integers, A[i].  
+// Next, Q lines contain the queries. Each line contains one character, followed by two space-separated integers. 
+// For the query on range, the character will be q and for the update query, the character will be u. 
 
-Constraints:
-1 ≤ i ≤ N
-0 ≤ B ≤ 10^8
-1 ≤ i <= j ≤ N
+// Constraints:
+// 1 ≤ i ≤ N
+// 0 ≤ B ≤ 10^8
+// 1 ≤ i <= j ≤ N
 
-Output Format:
-For each query on range, print the minimum number between the range, in a new line.
-Sample Input 1:
-4 3
-5 2 4 3
-q 1 3
-u 1 1
-q 3 4
+// Output Format:
+// For each query on range, print the minimum number between the range, in a new line.
+// Sample Input 1:
+// 4 3
+// 5 2 4 3
+// q 1 3
+// u 1 1
+// q 3 4
 
-Sample Output 1:
-2
-3
+// Sample Output 1:
+// 2
+// 3
 
-Solution - 
+// Solution - 
 
 #include<bits/stdc++.h>
 using namespace std;

c6ccfd2f3bb04502bd79e7d5239338c820ffec7f
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-08 14:25:01 +0530

    Create number_pattern_1.cpp

---
+++ 1_conditionals_and_loops/number_pattern_1.cpp
@@ -0,0 +1,37 @@
+// Print the following pattern
+// Pattern for N = 4
+
+// 1
+// 23
+// 345
+// 4567
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    int i = 1;
+    int j =1;
+    int k = 1;
+    while(i<=n){
+        k = i;
+        j = 1;
+        while(j<=i){
+            cout<<k;
+            k++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+   return 0;
+}
+
+

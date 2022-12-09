d17bd4a2584061d25f22ba030976f6ccff117474
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-09 10:38:14 +0530

    Create 1_square_pattern.cpp

---
+++ 2_pattern/1_square_pattern.cpp
@@ -0,0 +1,34 @@
+//  Code : Square Pattern
+// Send Feedback
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+
+// 4444
+// 4444
+// 4444
+// 4444
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    int i = 1;
+    int j = 1;
+    while(i<=n){
+        j = 1;
+        while(j<=n){
+            cout<<n;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+  
+}

0df274a66c283e32c6c57f2b0bf4c2a594181e8c
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-09 11:04:47 +0530

    Create 6_character_pattern.cpp

---
+++ 2_pattern/6_character_pattern.cpp
@@ -0,0 +1,36 @@
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+
+// A
+// BC
+// CDE
+// DEFG
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    int i = 1;
+    int j = 1;
+    char c = 'A';
+    while(i<=n){
+        j = 0;
+        while(j<i){
+            cout<<char(c+j);
+            j++;
+        }
+        c++;
+        cout<<endl;
+        i++;
+    }
+}
+
+

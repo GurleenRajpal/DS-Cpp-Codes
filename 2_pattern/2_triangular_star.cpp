0aa991d07695916c5caa3553c88b5cd0dbfca585
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-09 11:10:35 +0530

    Update 2_triangular_star.cpp

--- 2_pattern/2_triangular_star.cpp
+++ 2_pattern/2_triangular_star.cpp
@@ -1,10 +1,10 @@
-Print the following pattern for the given N number of rows.
-Pattern for N = 4
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
 
-*
-**
-***
-****
+// *
+// **
+// ***
+// ****
 
 #include<iostream>
 using namespace std;

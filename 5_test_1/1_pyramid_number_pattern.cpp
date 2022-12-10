bb211a800573d3cb178c2c4723a7800402d88b90
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-10 11:59:18 +0530

    Create 1_pyramid_number_pattern.cpp

---
+++ 5_test_1/1_pyramid_number_pattern.cpp
@@ -0,0 +1,51 @@
+// Pyramid Number Pattern
+// Send Feedback
+// Print the following pattern for the given number of rows.
+
+// Sample Input :
+// 5
+// Sample Output :
+//     1
+//    212
+//   32123
+//  4321234
+// 543212345
+      
+      
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  // Write your code here  
+	int n;
+    cin>>n;
+    int i = 1;
+    int j = 1;
+    
+    while(i<=n){
+        int spaces = n-i;
+        while(spaces){
+            cout<<" ";
+            spaces--;
+        }
+        j = i;
+        
+        while(j){
+            cout<<j;
+            j--;
+        }
+        j=2;
+        while(j<=i){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        
+        i++;
+        
+    }
+  
+}
+

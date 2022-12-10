307273f778ee3f25e72c6d2c1fd1f7bb39200e2e
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-10 11:39:49 +0530

    Create 4_triangle_of_numbers.cpp

---
+++ 3_pattern/4_triangle_of_numbers.cpp
@@ -0,0 +1,58 @@
+// Code : Triangle of Numbers
+// Send Feedback
+// Print the following pattern for the given number of rows.
+  
+// Sample Input 1:
+// 5
+// Sample Output 1:
+//        1
+//       232
+//      34543
+//     4567654
+//    567898765
+         
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*  Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+    int n;
+    cin>>n;
+    int i = 1;
+    int j = 1;
+    
+    while(i<=n){
+        int spaces = n-i;
+        while(spaces){
+            cout<<" ";
+            spaces--;
+        }
+        
+    	j = i;
+        int k = i;
+        while(j){
+            cout<<k;
+            k++;
+            j--;
+        }
+        
+        j=i-1;
+        k--;
+        k--;
+        while(j){
+            cout<<k;
+            k--;
+            j--;
+        }
+        
+        cout<<endl;
+        i++;
+        
+    }
+    
+    
+    return 0;
+}

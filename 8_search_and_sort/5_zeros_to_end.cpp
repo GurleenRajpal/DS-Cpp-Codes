0b80d5977e642eddf1b8bce183dcad3b690379f9
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-28 17:56:38 +0530

    Create 5_zeros_to_end.cpp

---
+++ 8_search_and_sort/5_zeros_to_end.cpp
@@ -0,0 +1,24 @@
+// Push Zeros to end
+// Send Feedback
+// You have been given a random integer array/list(ARR) of size N. You have been required to 
+// push all the zeros that are present in the array/list to the end of it. Also, make sure to maintain the relative order of the non-zero elements.
+// Note:
+// Change in the input array/list itself. You don't need to return or print the elements.
+
+// You need to do this in one scan of array only. Don't use extra space.
+
+
+void pushZeroesEnd(int *input, int size)
+{
+    //Write your code here
+    int j = 0;
+    for(int i=0; i<size; i++){
+        if(input[i]!=0){
+            input[j] = input[i];
+			j++;
+        }
+    }
+    for(int i = j;i<size;i++){
+        input[i] = 0;
+    }
+}

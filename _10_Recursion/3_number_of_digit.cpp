60a3064c51578a17d38ff4e8acd45140137b00cb
Gurleen <gurleensinghrajpal1994@gmail.com>
2022-12-28 18:46:38 +0530

    Create 3_number_of_digit.cpp

---
+++ _10_Recursion/3_number_of_digit.cpp
@@ -0,0 +1,21 @@
+// Number of Digits
+// Send Feedback
+// Given the number 'n', find out and return the number of digits present in a number recursively.
+// Input Format :
+// Integer n
+// Output Format :
+// Count of digits
+// Constraints :
+// 1 <= n <= 10^6
+// Sample Input 1 :
+//  156
+// Sample Output 1 :
+// 3
+
+int count(int n){
+    if(n == 0){
+        return 0;
+    }
+    int ans =1+ count(n / 10);
+    return ans;
+}

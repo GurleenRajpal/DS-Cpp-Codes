f493a36f424c75665164b2aac17bdd7263f1d5e2
Dev Limbani <devlimbani09@gmail.com>
2023-01-18 23:03:45 -0600

    Changes to formatting

--- 1_conditionals_and_loops/1_check_case.cpp
+++ 1_conditionals_and_loops/1_check_case.cpp
@@ -40,22 +40,22 @@
 
 // -1
 
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-	// Write your code here
-	char c;
-    cin>>c;
-    if(c>=97 && c<=122){
-        cout<<0<<endl;
+    char c;
+    cin >> c;
+    
+    if(c >= 97 && c <= 122){
+        cout << 0 << endl;
     }
-    else if(c>=65 && c<=90){
-        cout<<1<<endl;
+    else if(c >= 65 && c <= 90){
+        cout << 1 << endl;
     }
     else{
-        cout<<-1<<endl;
+        cout << -1 << endl;
     }
     return 0;
 }
-

4772a10358ac4e9e70bff8e8a814d5c206ab6ff8
Dev Limbani <devlimbani09@gmail.com>
2023-01-19 00:09:49 -0600

    changes to formatting

--- 1_conditionals_and_loops/4_number_pattern_1.cpp
+++ 1_conditionals_and_loops/4_number_pattern_1.cpp
@@ -1,34 +1,34 @@
 // Print the following pattern
-// Pattern for N = 4
 
+// Pattern for N = 4
 // 1
 // 23
 // 345
 // 4567
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 int main(){
 
-       /*  Read input as specified in the question.
-	* Print output as specified in the question.
-	*/
+    /*  Read input as specified in the question.
+    *   Print output as specified in the question.
+    */
+	
     int n;
-    cin>>n;
-    int i = 1;
-    int j =1;
-    int k = 1;
-    while(i<=n){
+    cin >> n;
+    int i = 1, j = 1, k = 1;
+	
+    while(i <= n){
         k = i;
         j = 1;
-        while(j<=i){
-            cout<<k;
+	    
+        while(j <= i){
+            cout << k;
             k++;
             j++;
         }
-        cout<<endl;
+        cout << endl;
         i++;
     }
    return 0;

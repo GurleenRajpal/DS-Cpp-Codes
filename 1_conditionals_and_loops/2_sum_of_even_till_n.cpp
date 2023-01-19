b52945de2a3e29c865510ce2c7c5e90a827e6f2c
Dev Limbani <devlimbani09@gmail.com>
2023-01-18 23:10:17 -0600

    Changes to formatting

--- 1_conditionals_and_loops/2_sum_of_even_till_n.cpp
+++ 1_conditionals_and_loops/2_sum_of_even_till_n.cpp
@@ -1,42 +1,41 @@
 //  Sum of Even Numbers till N
 // Send Feedback
 // Given a number N, print sum of all even numbers from 1 to N.
-// Input Format :
 
+// Input Format :
 // Integer N
 
 // Output Format :
-
 // Required Sum 
 
 // Sample Input 1 :
-
 //  6
 
 // Sample Output 1 :
-
 // 12
 
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 int main(){
 
-       	/*  
-		Read input as specified in the question.
-		Print output as specified in the question.
-	*/
+    /*  
+	Read input as specified in the question.
+	Print output as specified in the question.
+    */
+	
     int n;
-    cin>>n;
+    cin >> n;
+	
     int sum = 0;
+	
     while(n){
-        if(n%2==0){
-            sum+=n;
+        if(n % 2 == 0){
+            sum += n;
         }
-        n--;
+        n -- ;
     }
-    cout<<sum<<endl;
+    cout << sum << endl;
   
 }
